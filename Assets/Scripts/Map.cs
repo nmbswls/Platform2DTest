97fe1b55e6fd7906c40c7cb0bdc42556dbfe99d4
陈烨轩 <chenyexuan@diyidan.com>
2019-10-15 12:51:34 -0700

    ?

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -1,18 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using UnityEngine;
 
 public class Map : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private MapFootHoldTree footholds = null;
+
+    //
 }
--- Assets/Scripts/MapFootHold.cs
+++ Assets/Scripts/MapFootHold.cs
@@ -7,6 +7,9 @@ public class MapFootHold : MonoBehaviour, IComparable<MapFootHold>
 {
     public Vector2Int P1;
     public Vector2Int P2;
+    //ss
+    private int id;
+    private int next, prev;
 
     public bool isWall()
     {
@@ -34,12 +37,47 @@ public class MapFootHold : MonoBehaviour, IComparable<MapFootHold>
         return P2.y;
     }
 
-
-
-
-
-    public int CompareTo(MapFootHold other)
+    public int getId()
     {
-        throw new NotImplementedException();
+        return id;
+    }
+
+    public int getNext()
+    {
+        return next;
+    }
+
+    public void setNext(int next)
+    {
+        this.next = next;
+    }
+
+    public int getPrev()
+    {
+        return prev;
+    }
+
+    public void setPrev(int prev)
+    {
+        this.prev = prev;
+    }
+
+
+
+    public int CompareTo(MapFootHold o)
+    {
+        MapFootHold other = (MapFootHold)o;
+        if (P2.y < other.getY1())
+        {
+            return -1;
+        }
+        else if (P1.y > other.getY2())
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
     }
 }
